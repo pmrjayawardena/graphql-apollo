1f47b4cb85714d24b31758e7c6129713b4b4c43f
Prabodha Jayawardena <prabodha@dev.bukflights.com>
2022-06-15 19:29:24 +0530

    -added graphql and queried data from external graphql server and added in category component

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,27 +1,53 @@
 import { createContext, useState, useEffect } from 'react';
-
+import { gql, useQuery } from '@apollo/client';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 export const CategoriesContext = createContext({
-  categoriesMap: {},
+	categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+	query {
+		collections {
+			id
+			title
+			items {
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+	const { loading, error, data } = useQuery(COLLECTIONS);
+	const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+	console.log(loading);
+	console.log(data);
 
-    getCategoriesMap();
-  }, []);
+	useEffect(() => {
+		if (data) {
+			const { collections } = data;
+			const collectionMap = collections.reduce((acc, collection) => {
+				const { title, items } = collection;
+				acc[title.toLowerCase()] = items;
+				return acc;
+			}, {});
+			setCategoriesMap(collectionMap);
+		}
+	}, [data]);
+	// useEffect(() => {
+	//   const getCategoriesMap = async () => {
+	//     const categoryMap = await getCategoriesAndDocuments();
+	//     setCategoriesMap(categoryMap);
+	//   };
 
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
+	//   getCategoriesMap();
+	// }, []);
+
+	const value = { categoriesMap, loading };
+	return (
+		<CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+	);
 };
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,57 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
 import ProductCard from '../../components/product-card/product-card.component';
-
+import { gql, useQuery } from '@apollo/client';
 import { CategoriesContext } from '../../contexts/categories.context';
-
+import Spinner from '../../components/spinner/spinner.component';
 import { CategoryContainer, Title } from './category.styles';
 
+const GET_CATEGORY = gql`
+	query ($title: String) {
+		getCollectionsByTitle(title: $title) {
+			id
+			items {
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
 const Category = () => {
-  const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+	const { category } = useParams();
+	const [products, setProducts] = useState([]);
+	const { loading, error, data } = useQuery(GET_CATEGORY, {
+		variables: {
+			title: category,
+		},
+	});
 
-  useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+	useEffect(() => {
+		if (data) {
+			const { getCollectionsByTitle } = data;
 
-  return (
-    <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
-    </Fragment>
-  );
+			setProducts(getCollectionsByTitle.items);
+		}
+	}, [data]);
+
+	return (
+		<Fragment>
+			{loading ? (
+				<Spinner />
+			) : (
+				<>
+					<Title>{category.toUpperCase()}</Title>
+					<CategoryContainer>
+						{products &&
+							products.map((product) => (
+								<ProductCard key={product.id} product={product} />
+							))}
+					</CategoryContainer>
+				</>
+			)}
+		</Fragment>
+	);
 };
 
 export default Category;
